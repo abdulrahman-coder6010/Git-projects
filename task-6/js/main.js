2da5d672f75b9e04813b8236f8d0e765e30cebf7
abdulrahman-coder6010 <abdulrahamangamal@gmail.com>
2020-09-11 14:26:02 +0200

    modification in css and js files

--- task-6/js/main.js
+++ task-6/js/main.js
@@ -5,8 +5,10 @@ document.addEventListener("scroll",()=>{
 var scroll_position =window.scrollY;
 if (scroll_position > 250) {
     header.style.backgroundColor ="#29323c";
+    document.querySelector("#header > div > div > div.brand > a > h1").style.color = "#fff";
 } else {
     header.style.backgroundColor ="transparent"
+    document.querySelector("#header > div > div > div.brand > a > h1").style.color = "#000"
 }
 })
 
