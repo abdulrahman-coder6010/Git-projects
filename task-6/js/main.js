07d97beb6e0aa4ffae9b1c50d8c1976ce2813483
abdulrahman-coder6010 <abdulrahamangamal@gmail.com>
2020-09-14 21:01:23 +0200

    added task-7 and modification in task-6

--- task-6/js/main.js
+++ task-6/js/main.js
@@ -6,9 +6,19 @@ var scroll_position =window.scrollY;
 if (scroll_position > 250) {
     header.style.backgroundColor ="#29323c";
     document.querySelector("#header > div > div > div.brand > a > h1").style.color = "#fff";
+    document.querySelector("#header > div > div > div.nav-list > ul > li > a.nav1").style.color = "#fff";
+    document.querySelector("#header > div > div > div.nav-list > ul > li > a.nav2").style.color = "#fff";
+    document.querySelector("#header > div > div > div.nav-list > ul > li > a.nav3").style.color = "#fff";
+    document.querySelector("#header > div > div > div.nav-list > ul > li > a.nav4").style.color = "#fff";
+    document.querySelector("#header > div > div > div.nav-list > ul > li > a.nav5").style.color = "#fff";
 } else {
-    header.style.backgroundColor ="transparent"
-    document.querySelector("#header > div > div > div.brand > a > h1").style.color = "#000"
+    header.style.backgroundColor ="transparent";
+    document.querySelector("#header > div > div > div.brand > a > h1").style.color = "#000";
+    document.querySelector("#header > div > div > div.nav-list > ul > li > a.nav1").style.color = "#000";
+    document.querySelector("#header > div > div > div.nav-list > ul > li > a.nav2").style.color = "#000";
+    document.querySelector("#header > div > div > div.nav-list > ul > li > a.nav3").style.color = "#000";
+    document.querySelector("#header > div > div > div.nav-list > ul > li > a.nav4").style.color = "#000";
+    document.querySelector("#header > div > div > div.nav-list > ul > li > a.nav5").style.color = "#000";
 }
 })
 
